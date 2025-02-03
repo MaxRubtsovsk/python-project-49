64946a804b19f611ba24a1bb9e4bc8f1f6b926db
Max Abrashchenko <max.abrashchenko@gmail.com>
2025-02-03 21:48:31 +0300

    removed extra function call

--- brain_games/sly.py
+++ brain_games/sly.py
@@ -6,21 +6,22 @@ def is_even():
     print('Hello, ', name, '!', sep='')
     print('Answer "yes" if the number is even, otherwise answer "no".')
     cnt = 0
-    while cnt < 3:
-        random_number = randint(-9999999999, 9999999999)
-        print('Question:', random_number)
-        answer = input()
-        if random_number % 2 == 0 and answer == 'yes' or random_number % 2 != 0 and answer == 'no':
-            print('Correct!')
-            cnt += 1
-        else:
-            if random_number % 2 == 0:
-                print("'no' is wrong answer ;(. Correct answer was 'yes'.", f"Let's try again, {name}!", sep='\n')
-                break
+    flag = True
+    while flag == True:
+        while cnt < 3:
+            random_number = randint(-9999999999, 9999999999)
+            print('Question:', random_number)
+            answer = input()
+            if random_number % 2 == 0 and answer == 'yes' or random_number % 2 != 0 and answer == 'no':
+                print('Correct!')
+                cnt += 1
             else:
-                print("'yes' is wrong answer ;(. Correct answer was 'no'.", f"Let's try again, {name}!", sep='\n')
-                break
-        if cnt == 3:
-            print('Congratulations, ', name, '!', sep='')
-        flag = False
-is_even()
+                if random_number % 2 == 0:
+                    print("'no' is wrong answer ;(. Correct answer was 'yes'.", f"Let's try again, {name}!", sep='\n')
+                    break
+                else:
+                    print("'yes' is wrong answer ;(. Correct answer was 'no'.", f"Let's try again, {name}!", sep='\n')
+                    break
+            if cnt == 3:
+                print('Congratulations, ', name, '!', sep='')
+            flag = False
